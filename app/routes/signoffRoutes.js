98fc67e3c1f4c27effd219a645584ed83d3a7542
kenyonyozi <kenyonyozibwengye@gmail.com>
2022-06-07 21:40:36 +0300

    refactor to us mongo online

--- app/routes/signoffRoutes.js
+++ app/routes/signoffRoutes.js
@@ -8,7 +8,7 @@ router.use (expressValidator());
 
 
 //handles fetching client data from the db to populate the table
-router.get('/signoff', async(req,res)=>{
+router.get('/', async(req,res)=>{
     // to pick data from the 
     try {
         // helps return all the members in the collection clients
@@ -27,7 +27,7 @@ router.get('/signoff', async(req,res)=>{
 });
 // handling routes of dash for post to access reg form on route /dash
 // post from frontend using the dash route to backend 
-router.post('/signoff',(req,res)=>{
+router.post('/',(req,res)=>{
     // declaring the variables in the pug file name
     // we are requesting node js to foward the data and the body with the name given only uses name
     const name = req.body.name;
@@ -39,7 +39,7 @@ router.post('/signoff',(req,res)=>{
     // incase there is an error we serve back the form
     const errors = req.validationErrors();
     if (errors) {
-        res.render('/signoff')
+        res.render('/')
     }
     else{
         // new variable asssigin it 
@@ -61,7 +61,7 @@ router.post('/signoff',(req,res)=>{
                 // we first flash a message confirm save in data base
                 // go to dashboard since user signed up
                 console.log('data saved in database', );
-                res.redirect('/signoff')
+                res.redirect('/')
             }
         })
     }
